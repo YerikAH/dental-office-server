04e3fbb9731825e0c77f7acf223ec7f8ef0981e0
YerikAH <yerik05vh@gmail.com>
2024-01-03 17:37:48 -0500

    :tada: Start new project with MySQL

--- src/database.js
+++
@@ -1,9 +0,0 @@
-import { connect } from "mongoose";
-(async () => {
-  try {
-    const db = await connect("mongodb://localhost:27017/dental-office");
-    console.log("DB connected to", db.connection.name);
-  } catch (error) {
-    console.log(error);
-  }
-})();
--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 import express from "express";
-import morgan from "morgan";
 import cors from "cors";
-import { dirname, join } from "path";
-import { fileURLToPath } from "url";
+import {dirname, join} from 'path'
+import {fileURLToPath} from 'url'
 import { PORT } from "./config.js";
-import productsRoutes from "./routes/products.routes.js";
-import "./database.js";
+
 
 const app = express();
-// const __dirname = dirname(fileURLToPath(import.meta.url))
+const __dirname = dirname(fileURLToPath(import.meta.url));
+console.log(__dirname)
 
-app.use(morgan("dev"));
 app.use(cors());
-app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(productsRoutes);
+
+// app.use(indexRoutes);
+
+// app.use(express.static(join(__dirname, '../client/dist')))
 
 app.listen(PORT);
 console.log(`Server is listening on port ${PORT}`);
